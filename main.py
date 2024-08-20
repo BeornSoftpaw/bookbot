bd35a78b84b26f058060f86f6ec3f3ab80a0e03b
BeornSoftpaw <aloys@beornsoftpaw.de>
2024-08-20 15:50:41 +0200

    Finish

--- main.py
+++ main.py
@@ -3,14 +3,29 @@ def main():
     text = get_book_text(book_path)
     laenge = count_words(text)
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{laenge} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-    
+def count_words(text):
+    words = text.split()
+    return len(words)
+
 def get_chars_dict(text):
     chars = {}
     for c in text:
@@ -21,10 +36,14 @@ def get_chars_dict(text):
             chars[lowered] = 1
     return chars
 
+def sort_on(d):
+    return d["num"]
 
-def count_words(text):
-    words = text.split()
-    return len(words)
-
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 main()
